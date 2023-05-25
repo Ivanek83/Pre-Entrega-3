948fb12db325fb8304e6773d9529ee4d97589ba4
Ivan <ivanpablok@hotmail.com>
2023-05-24 22:56:44 -0300

    Creacion de las 3 clases en models.py

--- proyecto/apps/home/models.py
+++ proyecto/apps/home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300)
+    fecha = models.DateField()
+    def __str__(self):
+        return self.titulo
+    
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    def __str__(self):
+        return self.nombre
+    
+    
+class Medio(models.Model):
+    medio = models.CharField(max_length=100)
+    genero = models.CharField(max_length=30)
+    def __str__(self):
+        return self.medio
+    
+
+    
