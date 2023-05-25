b0c95b081aaf64d5218455879c8096bd816b8356
Ivan <ivanpablok@hotmail.com>
2023-05-24 23:14:36 -0300

    Registracion de modelos, creacion de admin y carga de datos de ejemplo

--- proyecto/apps/home/admin.py
+++ proyecto/apps/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Articulo)
+admin.site.register(models.Autor)
+admin.site.register(models.Medio)
+
